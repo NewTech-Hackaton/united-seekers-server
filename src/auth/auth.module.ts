4227d10db098bf6679aeb445d7790218d80c3bff
yurkenchik <142983974+yurkenchik@users.noreply.github.com>
2024-05-11 22:59:49 +0300

    created authorization bussiness logic and added DTOS

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,28 @@
-import { Module } from '@nestjs/common';
+import {forwardRef, Module} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import {JwtModule} from "@nestjs/jwt";
+import {TokensModule} from "../tokens/tokens.module";
+import {VolunteersModule} from "../volunteers/volunteers.module";
+import * as dotenv from "dotenv"
+dotenv.config()
+import * as process from "process"
 
 @Module({
   providers: [AuthService],
-  controllers: [AuthController]
+  controllers: [AuthController],
+  imports: [
+      forwardRef(() => TokensModule),
+      forwardRef(() => VolunteersModule),
+      JwtModule.register({
+          secret: "secret",
+          signOptions: {
+              expiresIn: "336h"
+          }
+      })
+  ],
+  exports: [
+      AuthService
+  ]
 })
 export class AuthModule {}
